97779c8c7cdff289f6524824cd9f59976c8a1525
Sreekanth <sreekanthps28@gmail.com>
2023-03-04 22:31:30 +0000

    Initial Commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by SREEKANTH PS on 04/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
